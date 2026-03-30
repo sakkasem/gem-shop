944efcbd168f4414199e2aea1cd23a54a3cc20a5
Ambatukam <117426666+sakkasem@users.noreply.github.com>
2026-03-30 08:10:01 +0700

    fix: cleanup server.js and fix case-sensitive paths

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://gem-shop-nu.vercel.app/'],
     credentials: true
 }));
 // server.js
